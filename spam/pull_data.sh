801dc93c4bcb23ef487a0724cacf19a4acee0ae9
caf <c.a.furia@gmail.com>
2019-09-09 15:40:43 +0200

    Delete after removal.

--- spam/pull_data.sh
+++ spam/pull_data.sh
@@ -3,6 +3,7 @@
 for f in 20030228_easy_ham.tar.bz2 20030228_easy_ham_2.tar.bz2 20030228_spam.tar.bz2 20030228_spam_2.tar.bz2 20021010_hard_ham.tar.bz2 20030228_hard_ham.tar.bz2; do
 	 wget "https://spamassassin.apache.org/old/publiccorpus/$f"
 	 tar xvjf "$f"
+	 rm "$f"
 done
 
 
