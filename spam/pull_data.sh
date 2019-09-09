c9aa87575620a0f90b14475b34d2586c0effb4b8
caf <c.a.furia@gmail.com>
2019-09-09 15:40:27 +0200

    Pull all spam data.

--- spam/pull_data.sh
+++ spam/pull_data.sh
@@ -1,4 +1,8 @@
 #! /bin/bash
 
-wget https://spamassassin.apache.org/old/publiccorpus/20030228_easy_ham.tar.bz2
-tar xvjf 20030228_easy_ham.tar.bz2
+for f in 20030228_easy_ham.tar.bz2 20030228_easy_ham_2.tar.bz2 20030228_spam.tar.bz2 20030228_spam_2.tar.bz2 20021010_hard_ham.tar.bz2 20030228_hard_ham.tar.bz2; do
+	 wget "https://spamassassin.apache.org/old/publiccorpus/$f"
+	 tar xvjf "$f"
+done
+
+
