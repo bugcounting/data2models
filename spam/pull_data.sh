b1f701892b800e375e9772b1cc84b094e1ed63ba
caf <c.a.furia@gmail.com>
2019-09-10 16:42:45 +0200

    Add other dataset to pull.

--- spam/pull_data.sh
+++ spam/pull_data.sh
@@ -6,4 +6,7 @@ for f in 20030228_easy_ham.tar.bz2 20030228_easy_ham_2.tar.bz2 20030228_spam.tar
 	 rm "$f"
 done
 
+wget "http://www.aueb.gr/users/ion/data/lingspam_public.tar.gz"
+tar xvf lingspam_public.tar.gz lingspam_public/bare
+rm lingspam_public.tar.gz
 
